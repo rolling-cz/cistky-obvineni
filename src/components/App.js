0457c0cbcf76bce59d0ddd5ebac9e7542ed31df0
Tomas Fechtner <fechtnertomas@gmail.com>
2020-05-04 19:01:45 +0200

    init

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
